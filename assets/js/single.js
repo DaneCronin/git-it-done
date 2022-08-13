964311934bcb9e24d3263c1546124008a38ac8f4
Dane Cronin <danecronin@gmail.com>
2022-08-13 10:08:27 -0600

    added function to check for issues and display to issues container

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,7 @@
+var issueContainerEl = document.querySelector("#issues-container"); //Variable storing Issues/pull requests and linking to ID of issues-container
+
 var getRepoIssues = function(repo) {
-    console.log(repo);
+   
     
     // variable to hold search for repo issues
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,7 +9,8 @@ var getRepoIssues = function(repo) {
         //request was successful
         if (response.ok) {
             response.json().then(function(data) {
-                console.log(data);
+                //pass response data to DOM function
+                displayIssues(data);
             });
         }
         else {
@@ -15,5 +18,42 @@ var getRepoIssues = function(repo) {
         }
     });
 }
+
+// function to accept Issues parameter and turn into DOM element
+var displayIssues = function(issues) {
+    //check to see if there are no open issues
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+    //create a link element to take users to that issue on GitHub
+    var issueEl = document.createElement("a");
+    issueEl.classList = "list-item flex-row justify-space-between align-center";
+    issueEl.setAttribute("href", issues[i].html_url);
+    issueEl.setAttribute("target", "_blank");
+
+    issueContainerEl.appendChild(issueEl);
+
+};
+
+//create span to hold issue title
+var titelEl = document.createElement("span");
+titelEl.textContent = issues[i].title;
+
+//append to container
+issueEl.appendChild(titleEl);
+
+//create a type element
+var typeEl = document.createElement("span");
+
+//check if issue is an actual issue or a pull request
+if (issues[i].pull_request) {
+    typeEl.textContent = "(Pull request)";
+} else {
+    typeEl.textContent = "(Issue)";
+}
+
+// append to container
+issueEl.appendChild(typeEl);
 
 getRepoIssues("facebook/react");
